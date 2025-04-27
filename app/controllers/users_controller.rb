0aa14f48cfca605c2ece870d910c53061f18d9d2
YuriHelene <yyuri.052917@gmail.com>
2025-04-27 18:37:45 +0900

    ページネーションをいいねと自投稿で分けた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
     # @tweets = @user.tweets
-    @tweet = Tweet.find_by(user_id:params[:id])
+    # @tweet = Tweet.find_by(user_id:params[:id])
+    # @likes = Like.where(user_id: @user.id)
+    # @liked_tweets = @user.liked_tweets
+    @tweets = @user.tweets.page(params[:page]).per(4)
+    @liked_tweets = @user.liked_tweets.page(params[:liked_page]).per(4)
   end
 
 end
