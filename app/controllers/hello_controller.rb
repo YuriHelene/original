3976aa4351ce57dc153e11c6bd54beebde6ebde8
YuriHelene <yyuri.052917@gmail.com>
2025-04-29 13:59:28 +0900

    トップページの新着投稿を6個に

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,7 +1,7 @@
 class HelloController < ApplicationController
   def top
     # 新規作成順に並び替えて、1件のモデルを取得
-    @new_tweets = Tweet.order(created_at: :desc).limit(3)
+    @new_tweets = Tweet.order(created_at: :desc).limit(6)
   end
   
 end
